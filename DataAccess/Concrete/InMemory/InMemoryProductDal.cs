f7ff943031a0944e77e527c56d83d79a648ef670
Yunus <83038643+YunusKaptan@users.noreply.github.com>
2022-01-20 00:31:18 +0100

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+
+        public InMemoryProductDal()
+        {//Oracle, sql server,  postgres,MongoDb
+            _products = new List<Product>
+            {
+                new Product {ProductId = 1,CategoryId = 1,ProductName = "Bardak",UnitPrice = 15,UnitsInStock = 15,},
+                new Product {ProductId = 2,CategoryId = 1,ProductName = "Kamera",UnitPrice = 500,UnitsInStock = 3,},
+                new Product {ProductId = 3,CategoryId = 1,ProductName = "Telefon",UnitPrice = 1500,UnitsInStock = 2,},
+                new Product {ProductId = 4,CategoryId = 1,ProductName = "Klavye",UnitPrice = 150,UnitsInStock = 65,},
+                new Product {ProductId = 5,CategoryId = 1,ProductName = "Fare",UnitPrice = 85,UnitsInStock = 1,}
+            };
+        }
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Update(Product product)
+        {//Gonderdigim urunun id sine sahip olan listedeki urunu bul
+            Product productToUpdate = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+
+        public void Delete(Product product)
+
+        { //LINQ Language Integrated Query
+
+            Product productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+
+            _products.Remove(productToDelete);
+
+            //LINQ OLMASAYDI
+
+            //Product productToDelete=null;
+            //foreach (var p in _products)
+            //{
+            //    if (product.ProductId==p.ProductId)
+            //    {
+            //        productToDelete = p;
+            //    }
+            //}
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p => p.CategoryId == categoryId).ToList();
+        }
+    }
+}
