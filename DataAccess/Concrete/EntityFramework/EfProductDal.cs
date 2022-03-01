b67ee75cfda64404fafc326f4e02a2ec4f73df98
Yunus <83038643+YunusKaptan@users.noreply.github.com>
2022-03-01 14:04:13 +0100

    Added Web API Layer

--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -11,24 +11,23 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal :EfEntityRepositoryBase<Product,NorthwindContext>,IProductDal
+    public class EfProductDal : EfEntityRepositoryBase<Product, NorthwindContext>, IProductDal
     {
-       
 
         public List<ProductDetailDto> GetProductDetails()
         {
             using (NorthwindContext context = new NorthwindContext())
             {
                 var result = from p in context.Products
-                    join c in context.Categories
-                        on p.CategoryId equals c.CategoryId
-                    select new ProductDetailDto
-                    {
-                        ProductId = p.ProductId,
-                        ProductName = p.ProductName,
-                        CategoryName = c.CategoryName,
-                        UnitsInStock = p.UnitsInStock
-                    };
+                             join c in context.Categories
+                                 on p.CategoryId equals c.CategoryId
+                             select new ProductDetailDto
+                             {
+                                 ProductId = p.ProductId,
+                                 ProductName = p.ProductName,
+                                 CategoryName = c.CategoryName,
+                                 UnitsInStock = p.UnitsInStock
+                             };
                 return result.ToList();
             }
         }
