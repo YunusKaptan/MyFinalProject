c180dfac78ca8363f524a463ad3fbba88c7b525f
Yunus <83038643+YunusKaptan@users.noreply.github.com>
2022-03-25 22:47:52 +0100

    User login and register dtos and AuthController added

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -15,7 +15,13 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError = "There can be a maximum of 10 products in a category.";
         public static string ProductUpdated = "Product updated";
         public static string ProductNameAlreadyExists = "Product name already exists";
-        public static string CategoryLimitExceeded="Category limit exceeded";
-        public static string AuthorizationDenied = " Authorization denied" ;
+        public static string CategoryLimitExceeded = "Category limit exceeded";
+        public static string AuthorizationDenied = " Authorization denied";
+        public static string UserRegistered = "User registered";
+        public static string UserNotFound = "User not found";
+        public static string PasswordError = "Password error";
+        public static string SuccessfullyLogin = "Successfully login";
+        public static string UserAlreadyExists = "User already exists";
+        public static string AccessTokenCreated = "AccessToken created ";
     }
 }
