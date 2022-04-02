acebfd95ca5dbd2c80ca759e3d46597a8c6519f2
Yunus <83038643+YunusKaptan@users.noreply.github.com>
2022-04-02 11:40:50 +0200

    Added Service collection extensions and CoreModule

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -13,6 +13,8 @@ using System.Threading.Tasks;
 using Autofac.Core;
 using Business.Abstract;
 using Business.Concrete;
+using Core.DependencyResolvers;
+using Core.Extensions;
 using Core.Utilities.IoC;
 using Core.Utilities.Security.JWT;
 using DataAccess.Abstract;
@@ -40,8 +42,6 @@ namespace WebAPI
             //services.AddSingleton<IProductService,ProductManager>();
             //services.AddSingleton<IProductDal,EfProductDal>();
 
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -58,7 +58,10 @@ namespace WebAPI
                         IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
                     };
                 });
-            ServiceTool.Create(services);
+            services.AddDependencyResolvers(new ICoreModule[]
+            {
+                new CoreModule()
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
