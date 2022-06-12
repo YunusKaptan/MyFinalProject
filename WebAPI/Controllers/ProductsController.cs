596dbdff0d78e7aeb467bd67cd554937452c165e
Yunus <83038643+YunusKaptan@users.noreply.github.com>
2022-06-12 15:55:15 +0200

    deleted thread sleep

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -32,8 +32,6 @@ namespace WebAPI.Controllers
             //Dependency chain
             //Swagger
 
-            Thread.Sleep(1000);
-
             var result = _productService.GetAll();
             if (result.Success)
             {
